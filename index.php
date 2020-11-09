ae7ce9abb138fa58c33f2ed4e94dd2c662720f9d
mcarob <37860253+mcarob@users.noreply.github.com>
2020-11-08 21:44:43 -0500

    todos para index

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
         <title> La Biblioteca </title>
         
         <!-- Favicon -->
-        <link href="images/favicon.ico" rel="icon" type="image/x-icon" />
-        
+        <link href="images/favicon.ico" rel="icon" type="image/x-icon" />   
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i%7CLato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />   
