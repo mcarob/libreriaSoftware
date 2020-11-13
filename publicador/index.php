ecd03bc9a619f9f72a2d787a909d102efe17a4d7
BadLook <andres.arevalorojas@outlook.com>
2020-11-12 21:30:53 -0500

    Nuevas rutas

---
+++ publicador/index.php
@@ -0,0 +1,262 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Home | Bookshop Responsive Bootstrap4 Template</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="assetsCliente/images/favicon.ico">
+	<link rel="apple-touch-icon" href="assetsCliente/images/icon.png">
+
+	<!-- Google font (font-family: 'Roboto', sans-serif; Poppins ; Satisfy) -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,600,600i,700,700i,800" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet"> 
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="../assetsCliente/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assetsCliente/css/plugins.css">
+	<link rel="stylesheet" href="../assetsCliente/style.css">
+
+	<!-- Cusom css -->
+   <link rel="stylesheet" href="../assetsCliente/css/custom.css">
+
+	<!-- Modernizer js -->
+	<script src="assetsCliente/js/vendor/modernizr-3.5.0.min.js"></script>
+</head>
+<body>
+
+
+	<!-- Main wrapper -->
+	<div class="wrapper" id="wrapper">
+		<!-- Header -->
+		<header id="wn__header" class="header__area header__absolute sticky__header">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-6 col-lg-2">
+						<div class="logo">
+							<a href="index.html">
+								<img src="assetsCliente/images/logo/logo.png" alt="logo images">
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-8 d-none d-lg-block">
+						<nav class="mainmenu__nav">
+							<ul class="meninmenu d-flex justify-content-start">
+								<li class="drop with--one--item"><a href="index.html">Inicio</a></li>
+								<li class="drop"><a href="#">Documentos</a>
+									<div class="megamenu mega02">
+										<ul class="item item02">
+											<li class="title">Documentos Físicos</li>
+											<li><a href="shop-grid.html">Libros Físicos</a></li>
+											<li><a href="single-product.html">Ponencias Físicas</a></li>
+											<li><a href="single-product.html">Articulos Físicas</a></li>
+										</ul>
+										<ul class="item item02">
+											<li class="title">Documentos Digitales</li>
+											<li><a href="shop-grid.html">Libros Digitales</a></li>
+											<li><a href="single-product.html">Ponencias Digitales</a></li>
+											<li><a href="single-product.html">Articulos Digitales</a></li>
+										</ul>
+									</div>
+								</li>
+								<li class="drop"><a href="shop-grid.html">La Libreria</a>
+									<div class="megamenu dropdown">
+										<ul class="item item01">
+											<li class="title">Nuestros Servicios</li>
+											<li><a href="shop-grid.html">Reserva Online Documentos </a></li>
+											<li><a href="shop-grid.html">Descargas de Documentos digitales </a></li>
+											<li><a href="shop-grid.html">Salas de conferencia </a></li>
+										</ul>
+									</div>
+								</li>
+								<li class="drop"><a href="shop-grid.html">Nuestras Sedes</a>
+									<div class="megamenu mega02">
+										<ul class="item item02">
+											<li class="title">Bogotá</li>
+											<li><a href="shop-grid.html">Suba Bulevar</a></li>
+											<li><a href="shop-grid.html">Unicentro</a></li>
+											<li><a href="shop-grid.html">Centro Mayor</a></li>
+										</ul>
+										<ul class="item item02">
+											<li class="title">Medellín</li>
+
+										</ul>
+									</div>
+								</li>
+							
+								<li><a href="contact.html">Contactanos</a></li>
+								<li><a href="contact.html">Registarse/Iniciar Sesión</a></li>
+							</ul>
+						</nav>
+					</div>
+					<div class="col-md-6 col-sm-6 col-6 col-lg-2">
+						<ul class="header__sidebar__right d-flex justify-content-end align-items-center">
+							<li class="shop_search"><a class="search__active" href="#"></a></li>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<!-- Start Mobile Menu -->
+				<div class="row d-none">
+					<div class="col-lg-12 d-none">
+						<nav class="mobilemenu__nav">
+							<ul class="meninmenu">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="#">Pages</a>
+									<ul>
+										<li><a href="about.html">About Page</a></li>
+										<li><a href="portfolio.html">Portfolio</a>
+											<ul>
+												<li><a href="portfolio.html">Portfolio</a></li>
+												<li><a href="portfolio-details.html">Portfolio Details</a></li>
+											</ul>
+										</li>
+										<li><a href="my-account.html">My Account</a></li>
+										<li><a href="cart.html">Cart Page</a></li>
+										<li><a href="checkout.html">Checkout Page</a></li>
+										<li><a href="wishlist.html">Wishlist Page</a></li>
+										<li><a href="error404.html">404 Page</a></li>
+										<li><a href="faq.html">Faq Page</a></li>
+										<li><a href="team.html">Team Page</a></li>
+									</ul>
+								</li>
+								<li><a href="shop-grid.html">Shop</a>
+									<ul>
+										<li><a href="shop-grid.html">Shop Grid</a></li>
+										<li><a href="single-product.html">Single Product</a></li>
+									</ul>
+								</li>
+								<li><a href="blog.html">Blog</a>
+									<ul>
+										<li><a href="blog.html">Blog Page</a></li>
+										<li><a href="blog-details.html">Blog Details</a></li>
+									</ul>
+								</li>
+								<li><a href="contact.html">Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				<!-- End Mobile Menu -->
+	            <div class="mobile-menu d-block d-lg-none">
+	            </div>
+	            <!-- Mobile Menu -->	
+			</div>		
+		</header>
+		<!-- //Header -->
+		<!-- Start Search Popup -->
+		<div class="brown--color box-search-content search_active block-bg close__top">
+			<form id="search_mini_form" class="minisearch" action="#">
+				<div class="field__search">
+					<input type="text" placeholder="Que tipo de documento prefieres">
+					<input type="text" placeholder="Buscar por palabra clave en nuestros documentos">
+					<div class="action">
+						<a href="#"><i class="zmdi zmdi-search"></i></a>
+					</div>
+				</div>
+			</form>
+			<div class="close__wrap">
+				<span>close</span>
+			</div>
+		</div>
+		<!-- End Search Popup -->
+        <!-- Start Slider area -->
+        <div class="slider-area brown__nav slider--15 slide__activation slide__arrow01 owl-carousel owl-theme">
+        	<!-- Start Single Slide -->
+	        <div class="slide animation__style10 bg-image--1index fullscreen align__center--left">
+	            <div class="container">
+	            	<div class="row">
+	            		<div class="col-lg-12">
+	            			<div class="slider__content">
+		            			<div class="contentbox">
+		            				<h2>La <span>Libreria </span></h2>
+		            				<h2>La mejor <span>Forma de </span></h2>
+		            				<h2><span>Buscar tus </span>Documentos </h2>
+				                   	<a class="shopbtn" href="#">Ver todos</a>
+		            			</div>
+	            			</div>
+	            		</div>
+	            	</div>
+	            </div>
+            </div>
+            <!-- End Single Slide -->
+        	<!-- Start Single Slide -->
+	        <div class="slide animation__style10 bg-image--7 fullscreen align__center--left">
+	            <div class="container">
+	            	<div class="row">
+	            		<div class="col-lg-12">
+	            			<div class="slider__content">
+		            			<div class="contentbox">
+		            				<h2>Buy <span>your </span></h2>
+		            				<h2>favourite <span>Book </span></h2>
+		            				<h2>from <span>Here </span></h2>
+				                   	<a class="shopbtn" href="#">shop now</a>
+		            			</div>
+	            			</div>
+	            		</div>
+	            	</div>
+	            </div>
+            </div>
+            <!-- End Single Slide -->
+        </div>
+        <!-- End Slider area -->
+		<!-- Escoger tipo de documento -->
+
+		<section class="search-filters">
+            <div class="container">
+                <div class="filter-box">
+                    <h3>¿Qué tipo de documento estas buscando?</h3>
+                    <form action="http://libraria.demo.presstigers.com/index.html" method="get">
+						<div class="row">
+                        <div class="col-md-3 col-sm-4">
+                            <div class="form-group">
+                                <label class="sr-only" for="keywords">Buscar por palabras claves</label>
+                                <input class="form-control" placeholder="Buscar por palabra Clave" id="keywords" name="keywords" type="text">
+                            </div>
+						</div>
+                        <div class="col-md-3 col-sm-4">
+                            <div class="form-group">
+                                <select name="catalog" id="catalog" class="form-control">
+                                    <option>Todos Los Documentos</option>
+                                    <option>Libro</option>
+                                    <option>Ponencias</option>
+                                    <option>Artículos Científicos</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-4">
+                            <div class="form-group">
+                                <select name="category" id="category" class="form-control">
+                                    <option>Todos los Recursos</option>
+                                    <option>Recurso Físico</option>
+                                    <option>Recurso Electrónico</option>
+
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-sm-4">
+                            <div class="form-group">
+                                <input class="form-control" type="submit" value="Search">
+                            </div>
+						</div>
+					</div>
+                    </form>
+                </div>
+            </div>
+        </section>
+	</div>
+	<!-- //Main wrapper -->
+
+	<!-- JS Files -->
+	<script src="../assetsCliente/js/vendor/jquery-3.2.1.min.js"></script>
+	<script src="../assetsCliente/js/popper.min.js"></script>
+	<script src="../assetsCliente/js/bootstrap.min.js"></script>
+	<script src="../assetsCliente/js/plugins.js"></script>
+	<script src="../assetsCliente/js/active.js"></script>
+	
+</body>
+</html>
