1a18a6ba0e2c83bd19ac492732afc64ea9772117
sebastianGR429 <55076154+sebastianGR429@users.noreply.github.com>
2020-11-13 01:35:40 -0500

    index

---
+++ footer.php
@@ -0,0 +1,66 @@
+<head>
+    <!-- Favicons -->
+	<link rel="shortcut icon" href="assetsCliente/images/favicon.ico">
+	<link rel="apple-touch-icon" href="assetsCliente/images/icon.png">
+
+	<!-- Google font (font-family: 'Roboto', sans-serif; Poppins ; Satisfy) -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,600,600i,700,700i,800" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet"> 
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="assetsCliente/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assetsCliente/css/plugins.css">
+	<link rel="stylesheet" href="assetsCliente/style.css">
+
+	<!-- Cusom css -->
+   <link rel="stylesheet" href="assetsCliente/css/custom.css">
+
+	<!-- Modernizer js -->
+	<script src="assetsCliente/js/vendor/modernizr-3.5.0.min.js"></script>
+</head>
+<footer id="wn__footer" class="footer__area bg__cat--8 brown--color">
+			<div class="footer-static-top">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="footer__widget footer__menu">
+								<div class="ft__logo">
+									<a href="index.html">
+										<img src="assetsCliente/images/logo/3.png" alt="logo">
+									</a>
+									<p>En el bosquecillo siempre encontraras tus libros, articulos y revistas preferidas.</p>
+								</div>
+								<div class="footer__content">
+									<ul class="social__net social__net--2 d-flex justify-content-center">
+										<li><a href="#"><i class="bi bi-facebook"></i></a></li>
+										<li><a href="#"><i class="bi bi-google"></i></a></li>
+										<li><a href="#"><i class="bi bi-twitter"></i></a></li>
+										<li><a href="#"><i class="bi bi-linkedin"></i></a></li>
+										<li><a href="#"><i class="bi bi-youtube"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="copyright__wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-12">
+							<div class="copyright">
+								<div class="copy__right__inner text-left">
+									<p>Copyright <i class="fa fa-copyright"></i> <a href="https://freethemescloud.com/">Free themes Cloud.</a> All Rights Reserved</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-12">
+							<div class="payment text-right">
+								<img src="assetsCliente/images/icons/payment.png" alt="" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
---
+++ index.php
@@ -0,0 +1,416 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Home | Bookshop Responsive Bootstrap4 Template</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="assetsCliente/images/favicon.ico">
+	<link rel="apple-touch-icon" href="assetsCliente/images/icon.png">
+
+	<!-- Google font (font-family: 'Roboto', sans-serif; Poppins ; Satisfy) -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,600,600i,700,700i,800" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet"> 
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="assetsCliente/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assetsCliente/css/plugins.css">
+	<link rel="stylesheet" href="assetsCliente/style.css">
+
+	<!-- Cusom css -->
+   <link rel="stylesheet" href="assetsCliente/css/custom.css">
+
+	<!-- Modernizer js -->
+	<script src="assetsCliente/js/vendor/modernizr-3.5.0.min.js"></script>
+</head>
+<body>
+	<!--[if lte IE 9]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+	<![endif]-->
+
+	<!-- Main wrapper -->
+	<div class="wrapper" id="wrapper">
+		<!-- Header -->
+		<?php
+		include("header.php");
+		?>
+		<!-- //Header -->
+		<!-- Start Search Popup -->
+		<div class="brown--color box-search-content search_active block-bg close__top">
+			<form id="search_mini_form" class="minisearch" action="#">
+				<div class="field__search">
+					<input type="text" placeholder="Que tipo de documento prefieres">
+					<input type="text" placeholder="Buscar por palabra clave en nuestros documentos">
+					<div class="action">
+						<a href="#"><i class="zmdi zmdi-search"></i></a>
+					</div>
+				</div>
+			</form>
+			<div class="close__wrap">
+				<span>close</span>
+			</div>
+		</div>
+		<!-- End Search Popup -->
+        <!-- Start Slider area -->
+        <div class="slider-area brown__nav slider--15 slide__activation slide__arrow01 owl-carousel owl-theme">
+        	<!-- Start Single Slide -->
+	        <div class="slide animation__style10 bg-image--1index fullscreen align__center--left">
+	            <div class="container">
+	            	<div class="row">
+	            		<div class="col-lg-12">
+	            			<div class="slider__content">
+		            			<div class="contentbox">
+		            				<h2>El <span>Bosquecillo</span> </h2>
+		            				<h2>La mejor <span>Forma de </span></h2>
+		            				<h2><span>Buscar tus </span>Documentos </h2>
+				                   	<a class="shopbtn" href="#">Ver todos</a>
+		            			</div>
+	            			</div>
+	            		</div>
+	            	</div>
+	            </div>
+            </div>
+            <!-- End Single Slide -->
+        	<!-- Start Single Slide -->
+	        <div class="slide animation__style10 bg-image--7 fullscreen align__center--left">
+	            <div class="container">
+	            	<div class="row">
+	            		<div class="col-lg-12">
+	            			<div class="slider__content">
+		            			<div class="contentbox">
+		            				<h2>Buy <span>your </span></h2>
+		            				<h2>favourite <span>Book </span></h2>
+		            				<h2>from <span>Here </span></h2>
+				                   	<a class="shopbtn" href="#">shop now</a>
+		            			</div>
+	            			</div>
+	            		</div>
+	            	</div>
+	            </div>
+            </div>
+            <!-- End Single Slide -->
+        </div>
+        <!-- End Slider area -->
+		<!-- Start BEst Seller Area -->
+
+		<section class="search-filters">
+            <div class="container">
+                <div class="filter-box">
+                    <h3>¿Qué tipo de documento estas buscando?</h3>
+                    <form action="http://libraria.demo.presstigers.com/index.html" method="get">
+						<div class="row">
+                        <div class="col-md-3 col-sm-4">
+                            <div class="form-group">
+                                <label class="sr-only" for="keywords">Buscar por palabras claves</label>
+                                <input class="form-control" placeholder="Buscar por palabra Clave" id="keywords" name="keywords" type="text">
+                            </div>
+						</div>
+                        <div class="col-md-3 col-sm-4">
+                            <div class="form-group">
+                                <select name="catalog" id="catalog" class="form-control">
+                                    <option>Todos Los Documentos</option>
+                                    <option>Libro</option>
+                                    <option>Ponencias</option>
+                                    <option>Artículos Científicos</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-4">
+                            <div class="form-group">
+                                <select name="category" id="category" class="form-control">
+                                    <option>Todos los Recursos</option>
+                                    <option>Recurso Físico</option>
+                                    <option>Recurso Electrónico</option>
+
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-sm-4">
+                            <div class="form-group">
+                                <input class="form-control" type="submit" value="Search">
+                            </div>
+						</div>
+					</div>
+                    </form>
+                </div>
+            </div>
+        </section>
+		<section class="wn__product__area brown--color pt--80  pb--30">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="section__title text-center">
+							<br><br>
+							<h2 class="title__be--2">New <span class="color--theme">Products</span></h2>
+							<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered lebmid alteration in some ledmid form</p>
+						</div>
+					</div>
+				</div>
+				<!-- Start Single Tab Content -->
+				<div class="furniture--4 border--round arrows_style owl-carousel owl-theme row mt--50">
+					<!-- Start Single Product -->
+					<div class="product product__style--3">
+						<div class="col-lg-3 col-md-4 col-sm-6 col-12">
+							<div class="product__thumb">
+							<a class="first__img" href="single-product.html"><img src="assetsCliente/images/books/1.jpg" width="270" height="340" alt="product image" ></a>
+								<a class="second__img animation1" href="single-product.html"><img src="assetsCliente/images/books/c.jpg" alt="product image" width="270" height="340"></a>
+								<div class="hot__box">
+									<span class="hot-label">BEST SALLER</span>
+								</div>
+							</div>
+							<div class="product__content content--center">
+								<h4><a href="single-product.html">robin parrish</a></h4>
+								<ul class="prize d-flex">
+									<li>$35.00</li>
+									<li class="old_prize">$35.00</li>
+								</ul>
+								<div class="action">
+									<div class="actions_inner">
+										<ul class="add_to_links">
+											<li><a class="cart" href="cart.html"><i class="bi bi-shopping-bag4"></i></a></li>
+											<li><a class="wishlist" href="wishlist.html"><i class="bi bi-shopping-cart-full"></i></a></li>
+											<li><a class="compare" href="#"><i class="bi bi-heart-beat"></i></a></li>
+											<li><a data-toggle="modal" title="Quick View" class="quickview modal-view detail-link" href="#productmodal"><i class="bi bi-search"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<div class="product__hover--content">
+									<ul class="rating d-flex">
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- Start Single Product -->
+					<!-- Start Single Product -->
+					<div class="product product__style--3">
+						<div class="col-lg-3 col-md-4 col-sm-6 col-12">
+							<div class="product__thumb">
+								<a class="first__img" href="single-product.html"><img src="assetsCliente/images/books/2.jpg" width="270" height="340" alt="product image" ></a>
+								<a class="second__img animation1" href="single-product.html"><img src="assetsCliente/images/books/c.jpg" alt="product image" width="270" height="340"></a>
+								<div class="hot__box color--2">
+									<span class="hot-label">HOT</span>
+								</div>
+							</div>
+							<div class="product__content content--center">
+								<h4><a href="single-product.html">The Remainng</a></h4>
+								<ul class="prize d-flex">
+									<li>$35.00</li>
+									<li class="old_prize">$35.00</li>
+								</ul>
+								<div class="action">
+									<div class="actions_inner">
+										<ul class="add_to_links">
+											<li><a class="cart" href="cart.html"><i class="bi bi-shopping-bag4"></i></a></li>
+											<li><a class="wishlist" href="wishlist.html"><i class="bi bi-shopping-cart-full"></i></a></li>
+											<li><a class="compare" href="#"><i class="bi bi-heart-beat"></i></a></li>
+											<li><a data-toggle="modal" title="Quick View" class="quickview modal-view detail-link" href="#productmodal"><i class="bi bi-search"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<div class="product__hover--content">
+									<ul class="rating d-flex">
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- Start Single Product -->
+					<!-- Start Single Product -->
+					<div class="product product__style--3">
+						<div class="col-lg-3 col-md-4 col-sm-6 col-12">
+							<div class="product__thumb">
+								<a class="first__img" href="single-product.html"><img src="assetsCliente/images/books/3.jpg" width="270" height="340" alt="product image"></a>
+								<a class="second__img animation1" href="single-product.html"><img src="assetsCliente/images/books/c.jpg" alt="product image" width="270" height="340"></a>
+								<div class="hot__box">
+									<span class="hot-label">BEST SALLER</span>
+								</div>
+							</div>
+							<div class="product__content content--center">
+								<h4><a href="single-product.html">Bowen Greenwood</a></h4>
+								<ul class="prize d-flex">
+									<li>$40.00</li>
+									<li class="old_prize">$35.00</li>
+								</ul>
+								<div class="action">
+									<div class="actions_inner">
+										<ul class="add_to_links">
+											<li><a class="cart" href="cart.html"><i class="bi bi-shopping-bag4"></i></a></li>
+											<li><a class="wishlist" href="wishlist.html"><i class="bi bi-shopping-cart-full"></i></a></li>
+											<li><a class="compare" href="#"><i class="bi bi-heart-beat"></i></a></li>
+											<li><a data-toggle="modal" title="Quick View" class="quickview modal-view detail-link" href="#productmodal"><i class="bi bi-search"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<div class="product__hover--content">
+									<ul class="rating d-flex">
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- Start Single Product -->
+					<!-- Start Single Product -->
+					<div class="product product__style--3">
+						<div class="col-lg-3 col-md-4 col-sm-6 col-12">
+							<div class="product__thumb">
+								<a class="first__img" href="single-product.html"><img src="assetsCliente/images/books/4.jpg" alt="product image" width="270" height="340"></a>
+								<a class="second__img animation1" href="single-product.html"><img src="assetsCliente/images/books/c.jpg" alt="product image" width="270" height="340"></a>
+								<div class="hot__box">
+									<span class="hot-label">HOT</span>
+								</div>
+							</div>
+							<div class="product__content content--center">
+								<h4><a href="single-product.html">Lando</a></h4>
+								<ul class="prize d-flex">
+									<li>$35.00</li>
+									<li class="old_prize">$50.00</li>
+								</ul>
+								<div class="action">
+									<div class="actions_inner">
+										<ul class="add_to_links">
+											<li><a class="cart" href="cart.html"><i class="bi bi-shopping-bag4"></i></a></li>
+											<li><a class="wishlist" href="wishlist.html"><i class="bi bi-shopping-cart-full"></i></a></li>
+											<li><a class="compare" href="#"><i class="bi bi-heart-beat"></i></a></li>
+											<li><a data-toggle="modal" title="Quick View" class="quickview modal-view detail-link" href="#productmodal"><i class="bi bi-search"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<div class="product__hover--content">
+									<ul class="rating d-flex">
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- Start Single Product -->
+					<!-- Start Single Product -->
+					<div class="product product__style--3">
+						<div class="col-lg-3 col-md-4 col-sm-6 col-12">
+							<div class="product__thumb">
+								<a class="first__img" href="single-product.html"><img src="assetsCliente/images/books/5.jpg" alt="product image" width="270" height="340"></a>
+								<a class="second__img animation1" href="single-product.html"><img src="assetsCliente/images/books/c.jpg" alt="product image" width="270" height="340"></a>
+								<div class="hot__box">
+									<span class="hot-label">HOT</span>
+								</div>
+							</div>
+							<div class="product__content content--center">
+								<h4><a href="single-product.html">Doctor Wldo</a></h4>
+								<ul class="prize d-flex">
+									<li>$35.00</li>
+									<li class="old_prize">$35.00</li>
+								</ul>
+								<div class="action">
+									<div class="actions_inner">
+										<ul class="add_to_links">
+											<li><a class="cart" href="cart.html"><i class="bi bi-shopping-bag4"></i></a></li>
+											<li><a class="wishlist" href="wishlist.html"><i class="bi bi-shopping-cart-full"></i></a></li>
+											<li><a class="compare" href="#"><i class="bi bi-heart-beat"></i></a></li>
+											<li><a data-toggle="modal" title="Quick View" class="quickview modal-view detail-link" href="#productmodal"><i class="bi bi-search"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<div class="product__hover--content">
+									<ul class="rating d-flex">
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- Start Single Product -->
+					<!-- Start Single Product -->
+					<div class="product product__style--3">
+						<div class="col-lg-3 col-md-4 col-sm-6 col-12">
+							<div class="product__thumb">
+								<a class="first__img" href="single-product.html"><img src="assetsCliente/images/books/6.jpg" alt="product image" width="270" height="340"></a>
+								<a class="second__img animation1" href="single-product.html"><img src="assetsCliente/images/books/c.jpg" alt="product image" width="270" height="340"></a>
+								<div class="hot__box">
+									<span class="hot-label">BEST SALER</span>
+								</div>
+							</div>
+							<div class="product__content content--center content--center">
+								<h4><a href="single-product.html">Ghost</a></h4>
+								<ul class="prize d-flex">
+									<li>$50.00</li>
+									<li class="old_prize">$35.00</li>
+								</ul>
+								<div class="action">
+									<div class="actions_inner">
+										<ul class="add_to_links">
+											<li><a class="cart" href="cart.html"><i class="bi bi-shopping-bag4"></i></a></li>
+											<li><a class="wishlist" href="wishlist.html"><i class="bi bi-shopping-cart-full"></i></a></li>
+											<li><a class="compare" href="#"><i class="bi bi-heart-beat"></i></a></li>
+											<li><a data-toggle="modal" title="Quick View" class="quickview modal-view detail-link" href="#productmodal"><i class="bi bi-search"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<div class="product__hover--content">
+									<ul class="rating d-flex">
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li class="on"><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+										<li><i class="fa fa-star-o"></i></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<!-- Start Single Product -->
+					</div>
+				</div>
+				<!-- End Single Tab Content -->
+			</div>
+		</section>
+		<!-- Start BEst Seller Area -->
+		<!-- Start NEwsletter Area -->
+		
+		<!-- Best Sale Area Area -->
+		<!-- Footer Area -->
+		<?php
+		include('footer.php')
+		?>
+		<!-- //Footer Area -->
+		<!-- QUICKVIEW PRODUCT -->
+		
+		<!-- END QUICKVIEW PRODUCT -->
+	</div>
+	<!-- //Main wrapper -->
+
+	<!-- JS Files -->
+	<script src="assetsCliente/js/vendor/jquery-3.2.1.min.js"></script>
+	<script src="assetsCliente/js/popper.min.js"></script>
+	<script src="assetsCliente/js/bootstrap.min.js"></script>
+	<script src="assetsCliente/js/plugins.js"></script>
+	<script src="assetsCliente/js/active.js"></script>
+	
+</body>
+</html>
