af1da3a2f7d9124e29fcc742c1d3ca65ed26eac5
acubilloso <acubilloso@unbosque.edu.co>
2020-11-18 19:59:29 -0500

    Empleado

--- empleado/TablaLibrosD.php
+++ empleado/TablaLibrosD.php
@@ -1,160 +1,111 @@
 <?php
-include ('Header.php');
-include ('menuEm.php');
+include('Header.php');
+include('menuEm.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/libreriaSoftware/controlador/ControladorDocumento.php');
+$CDocumentos = new ControladorDocumento();
+$digitales = $CDocumentos->listarDocumentoD();
 ?>
-
 <div class="mobile-menu-overlay"></div>
 
 <div class="main-container">
-		<div class="pd-ltr-20 xs-pd-20-10">
-			<div class="min-height-200px">
-				<div class="page-header">
-					<div class="row">
-						<div class="col-md-6 col-sm-12">
-							<div class="title">
-								<h4>Libros de Bosquecillo </h4>
-							</div>
-							<nav aria-label="breadcrumb" role="navigation">
-								<ol class="breadcrumb">
-									<li class="breadcrumb-item"><a href="index.html">Empleados</a></li>
-									<li class="breadcrumb-item active" aria-current="page">Listar</li>
-								</ol>
-							</nav>
+	<div class="pd-ltr-20 xs-pd-20-10">
+		<div class="min-height-200px">
+			<div class="page-header">
+				<div class="row">
+					<div class="col-md-6 col-sm-12">
+						<div class="title">
+							<h4>Documentos de Bosquecillo </h4>
 						</div>
-					
+						<nav aria-label="breadcrumb" role="navigation">
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href="index.html">Empleados</a></li>
+								<li class="breadcrumb-item active" aria-current="page">Listar</li>
+							</ol>
+						</nav>
 					</div>
+
 				</div>
-				
-			
-				
-				<!-- Checkbox select Datatable End -->
-				<!-- Export Datatable start -->
-				<div class="card-box mb-30">
-					<div class="pd-20">
-						<h4 class="text-blue h4">Libros Digitales</h4>
-					</div>
-					<div class="pb-20">
-						<table class="table hover multiple-select-row data-table-export nowrap">
-							<thead>
-								<tr>
-									<th class="table-plus datatable-nosort">Name</th>
-									<th>Age</th>
-									<th>Office</th>
-									<th>Address</th>
-									<th>Start Date</th>
-									<th>Salart</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td class="table-plus">Gloria F. Mead</td>
-									<td>25</td>
-									<td>Sagittarius</td>
-									<td>2829 Trainer Avenue Peoria, IL 61602 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>30</td>
-									<td>Gemini</td>
-									<td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>20</td>
-									<td>Gemini</td>
-									<td>2829 Trainer Avenue Peoria, IL 61602 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>30</td>
-									<td>Sagittarius</td>
-									<td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>25</td>
-									<td>Gemini</td>
-									<td>2829 Trainer Avenue Peoria, IL 61602 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>20</td>
-									<td>Sagittarius</td>
-									<td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>18</td>
-									<td>Gemini</td>
-									<td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>30</td>
-									<td>Sagittarius</td>
-									<td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>30</td>
-									<td>Sagittarius</td>
-									<td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>30</td>
-									<td>Gemini</td>
-									<td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>30</td>
-									<td>Gemini</td>
-									<td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-								<tr>
-									<td class="table-plus">Andrea J. Cagle</td>
-									<td>30</td>
-									<td>Gemini</td>
-									<td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
-									<td>29-03-2018</td>
-									<td>$162,700</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
-				<!-- Export Datatable End -->
 			</div>
-			<div class="footer-wrap pd-20 mb-20 card-box">
-				DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">Ankit Hingarajiya</a>
+
+
+
+			<!-- Checkbox select Datatable End -->
+			<!-- Export Datatable start -->
+			<div class="card-box mb-30">
+				<div class="pd-20">
+					<h4 class="text-blue h4">Documentos Digitales</h4>
+				</div>
+				<div class="pb-20">
+					<table class="table hover multiple-select-row data-table-export nowrap">
+						<thead>
+							<tr>
+								<th class="table-plus datatable-nosort">Titulo</th>
+								<th>Publicador</th>
+								<th>Editorial</th>
+								<th>Código</th>
+								<th>Tipo de documento</th>
+								<th>Acciones</th>
+							</tr>
+						</thead>
+						<tbody>
+								<?php
+								foreach ($digitales as $key) {
+									echo ("<tr>");
+									echo ("<td>" . $key['titulo_documento'] . "</td>");
+									echo ("<td>" . $key['nom_publicador'] . "</td>");
+									echo ("<td>" . $key['editorial_publicacion'] . "</td>");
+									if ($key['cod_tipo_documento'] == 3) {
+										echo ("<td>" . $key['informacion_ssn'] . "</td>");
+									} else {
+										echo ("<td> No </td>");
+									}
+									echo ("<td>" . $key['nom_tipo_documento'] . "</td>");
+									echo ("<td>
+										<div class='btn-list'>	
+											<button type='button' class='btn btn-outline-success' onclick='vermas(" . '"' . $key['cod_documento'] . '"' . ")'>Ver más</button>
+											</div>
+										</td>");
+
+
+								?>
+
+								<?php
+									echo ("</tr>");
+								}
+								?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<!-- Export Datatable End -->
+		</div>
+		<div class="footer-wrap pd-20 mb-20 card-box">
+			DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">Ankit Hingarajiya</a>
+		</div>
+	</div>
+
+
+	<div class="modal fade" id="modal11" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+			
+						
 			</div>
 		</div>
+	</div>
 </div>
 
 
+
+
+
+
+<script>
+	function vermas(valor) {
+		$('.modal-content').load('modalLibroD.php?id='+valor) 
+		$('#modal11').modal('show');
+	}
+</script>
 
 <script src="../TemplateAdministrador/vendors/scripts/core.js"></script>
 <script src="../TemplateAdministrador/vendors/scripts/script.min.js"></script>
@@ -174,3 +125,4 @@ include ('menuEm.php');
 <script src="../TemplateAdministrador/src/plugins/datatables/js/vfs_fonts.js"></script>
 <!-- Datatable Setting js -->
 <script src="../TemplateAdministrador/vendors/scripts/datatable-setting.js"></script>
+</body>
