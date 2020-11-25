06cbcbb6e87792a7ec826f33a58e597fb637bc30
gamendez <70824177+gamendez@users.noreply.github.com>
2020-11-25 17:52:53 -0500

    .

--- publicador/agregar_ponencia.php
+++ publicador/agregar_ponencia.php
@@ -4,21 +4,21 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/libreriaSoftware/controlador/Controlado
 include_once($_SERVER['DOCUMENT_ROOT'].'/libreriaSoftware/modelo/entidades/Documento.php');
 
 $tipo;
-if($_POST["tipo"] == 2)
+if($_POST["tipo"] == 3)
 {
     $tipo = $_POST["tipo"];
 }
 
-    $documento = ($_FILES['archivoDocumentoArt']['tmp_name']);  
-    if (isset($_FILES['archivoDocumentoArt'])) {
+    $documento = ($_FILES['archivoDocumentoPo']['tmp_name']);  
+    if (isset($_FILES['archivoDocumentoPo'])) {
         print_r("Entro aca");
-        if (($_FILES['archivoDocumentoArt']['type']) == 'application/pdf') {
+        if (($_FILES['archivoDocumentoPo']['type']) == 'application/pdf') {
             print_r("Entro aca 2");
-            if ((($_FILES['archivoDocumentoArt']['type']) == 'application/pdf') || (($_FILES['portadaArt']['type']) == 'image/jpeg')) {
+            if ((($_FILES['archivoDocumentoPo']['type']) == 'application/pdf') || (($_FILES['portadaPo']['type']) == 'image/jpeg')) {
                 print_r("Entro aca 3");
                 try {
-                    $datadocumento = $_FILES['archivoDocumentoArt']['tmp_name'];
-                    $dataportada = ($_FILES['portadaArt']['tmp_name']);
+                    $datadocumento = $_FILES['archivoDocumentoPo']['tmp_name'];
+                    $dataportada = ($_FILES['portadaPo']['tmp_name']);
                     if (($datadocumento == null)) {
                         echo ("Error al cargar el archivo del llibro");
                     } else {
@@ -37,16 +37,16 @@ if($_POST["tipo"] == 2)
                
                        // registrar empresa, paso las validaciones
                        /* ni_empresa ,nombre ,ccmpdf ,descripccion,logo,telefono,correo ,nomc,apellc ,telc,cargoc,correoc ,userempresa , passw ) */
-                        $documento = ($_FILES['archivoDocumentoArt']['tmp_name']);
-                        $pnameD = rand(1000,10000)."-".$_FILES["archivoDocumentoArt"]["name"];
-                        $tnameD = $_FILES["archivoDocumentoArt"]["tmp_name"];
+                        $documento = ($_FILES['archivoDocumentoPo']['tmp_name']);
+                        $pnameD = rand(1000,10000)."-".$_FILES["archivoDocumentoPo"]["name"];
+                        $tnameD = $_FILES["archivoDocumentoPo"]["tmp_name"];
                         $uploads_dir_doc = $_SERVER['DOCUMENT_ROOT'] . '/libreriaSoftware/archivos/documentos';
                         $seMovioDoc=move_uploaded_file($tnameD, $uploads_dir_doc.'/'.$pnameD);
 
                         //portada
-                        $portada = ($_FILES['portadaArt']['tmp_name']);
-                        $pnameP = rand(1000,10000)."-".$_FILES["portadaArt"]["name"];
-                        $tnameP = $_FILES["portadaArt"]["tmp_name"];
+                        $portada = ($_FILES['portadaPo']['tmp_name']);
+                        $pnameP = rand(1000,10000)."-".$_FILES["portadaPo"]["name"];
+                        $tnameP = $_FILES["portadaPo"]["tmp_name"];
                         $uploads_dir_por = $_SERVER['DOCUMENT_ROOT'] . '/libreriaSoftware/archivos/portadas';
                         $seMovioPor=move_uploaded_file($tnameP, $uploads_dir_por.'/'.$pnameP);
                        
@@ -54,7 +54,8 @@ if($_POST["tipo"] == 2)
                             $_POST["titulo"],
                             $_POST["fechaPublicacion"],
                             $_POST["editorial"],
-                            $_POST["ssn"],
+                            $_POST["isbn"],
+                            $_POST["nomCongreso"],
                             $_POST["idioma"],                           
                             $pnameD,
                             $pnameP
@@ -62,9 +63,9 @@ if($_POST["tipo"] == 2)
                       
                         print_r("Entro");
                         $controlador = new ControladorDocumento();
-                        $documento = new Documento(0,$datos[4],$tipo,2,$datos[0],                                 
-                                                         $datos[1],$datos[2],"",
-                                                         ""," ",$datos[3]," ",$pnameD,1,$pnameP);
+                        $documento = new Documento(0,$datos[5],$tipo,1,$datos[0],                                 
+                                                         $datos[1],$datos[2],$datos[3],
+                                                         "",$datos[4] ," "," ",$pnameD,1,$pnameP);
                         echo($controlador->agregarRegistro($documento));
                       
                     
--- publicador/regPresentacion.php
+++ publicador/regPresentacion.php
@@ -4,26 +4,17 @@
     <div class="wizard-card">
         <br>
         <br>
-       <!-- <div class="picture-container">
-            <div class="picture">
-                <img src="assetsCliente/images/icons/default-avatar1.png" class="picture-src" id="wizardPicturePreview" title="" />
-                <input type="file" id="portada" name="portada" required>
-            </div>
-            <h6>Elegir portada del titulo</h6>
-        </div>-->
-        <br>
-        <br>
-        <div class="row">
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">Cargar</span>
+        <div class="wizard-card">
+            <div class="picture-container">
+                <div class="picture">
+                    <img src="assetsCliente/images/logo/logo.png" class="picture-src" id="wizardPicturePreview3" title="" />
+                    <input type="file" id="portadaPo" name="portadaPo" required>
                 </div>
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="archivoDocumento">
-                    <label class="custom-file-label" for="inputGroupFile01">Elegir Documento</label>
-                </div>
+                <h6>Elegir portada</h6>
             </div>
         </div>
+        <br>
+        <br>
         <div class="row">
             <div class="col-md-4" aling="align-items-center">
                 <div class="form-group">
@@ -64,37 +55,56 @@
                     <input class="form-control" id="nomCongreso" name="nomCongreso">
                 </div>
             </div>
-            
-            
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label>Idioma:</label>
+                    <select class="custom-select" id="idioma" name="idioma">
+                        <option selected>Escoger idioma...</option>
+                        <option value="1">Inglés</option>
+                        <option value="2">Español</option>
+                        <option value="3">Portuges</option>
+                        <option value="4">Alemán</option>
+                        <option value="5">Frances</option>
+                    </select>
+                </div>
+            </div>
+
+
         </div>
         <div class="row">
-            
-            <div class="col-md-4">
-                
+
+            <div class="col-md-4" aling="align-items-center">
+
+                <div class="form-group">
+                    <label>Cargar archivo</label>
+                    <input type="file" class="form-control-file" id="archivoDocumentoPo" name="archivoDocumentoPo">
+                </div>
             </div>
-            
+
         </div>
     </div>
-    <input type= "number"class="form-control" id="tipo" name="tipo" value="3" hidden/>
+    <input type="number" class="form-control" id="tipo" name="tipo" value="3" hidden />
     <div class="form-footer pt-4 pt-5 mt-4" style="float: center;">
-            <a onclick="agregarPresentacion();" class="btn btn-success" value="Registrar">Registrar</a>
+        <a onclick="agregarPresentacion();" class="btn btn-success" value="Registrar">Registrar</a>
     </div>
-   
+
 </form>
 
 <script>
-  function agregarPresentacion() {
-        datos = $('#formNuevoPresentacion').serialize();
+    function agregarPresentacion() {
+        var myform = document.getElementById("formNuevoPresentacion");
+        var datos = new FormData(myform);
 
         $.ajax({
             type: "POST",
             data: datos,
-            url: "agregar_presentacion.php",
+            url: "agregar_ponencia.php",
+            processData: false,
+            contentType: false,
             success: function(r) {
 
                 console.log(r);
                 if (r == 1) {
-
                     window.location.href = "index.php";
                 } else {
 
