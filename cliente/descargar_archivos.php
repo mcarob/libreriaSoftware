666c09806d61c4d9420602a49fdad3da57f91ca0
sebastianGR429 <55076154+sebastianGR429@users.noreply.github.com>
2020-11-28 07:33:37 -0500

    cliente

--- cliente/descargar_archivos.php
+++ cliente/descargar_archivos.php
@@ -2,39 +2,38 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/libreriaSoftware/controlador/ControladorPeticionD.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/libreriaSoftware/modelo/entidades/peticion_digital.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/libreriaSoftware/controlador/ControladorDocumento.php');
-$conPeticionD=new ControladorPeticionDigital();
-$conDocumento=new ControladorDocumento();
-$existencias=$conDocumento->buscarExistenciaXdocumento($documento[1]);
 
 $documento=array(
     $_POST["cliente"],
     $_POST["idDocumento"]
 );
 
-for($i=0; $i<1;$i++)
-{
-    $existencia=$existencias[0];
-}
+$conPeticionD=new ControladorPeticionDigital();
+$conDocumento=new ControladorDocumento();
+$existencias=$conDocumento->buscarExistenciaXdocumento2($documento[1]);
 
-$peticion=new Peticion_digital(null,$existencia["cod_existencia_documento"],$documento[0],null);
-$conPeticionD->agregarRegistro($peticion);
+    for($i=0; $i<1;$i++)
+    {
+        $existencia=$existencias[0];
+    }
 
-$ruta="../archivos/documentos/".$_POST["rutaDoc"];
-$file = $ruta;
-$fp = fopen($file, "r") ;
+    $peticion=new Peticion_digital(null,$existencia["cod_existencia_documento"],$documento[0],null);
+    $conPeticionD->agregarRegistro($peticion);
+    $ruta="../archivos/documentos/".$_POST["rutaDoc"];
+    $file = $ruta;
+    $fp = fopen($file, "r") ;
 
-header("Cache-Control: maxage=1");
-header("Pragma: public");
-header("Content-type: application/pdf");
-header("Content-Disposition: inline; filename=".$_POST["rutaDoc"]."");
-header("Content-Description: PHP Generated Data");
-header("Content-Transfer-Encoding: binary");
-header('Content-Length:' . filesize($file));
-ob_clean();
-flush();
-while (!feof($fp)) {
-      $buff = fread($fp, 1024);
-      print $buff;
-}
-
+    header("Cache-Control: maxage=1");
+    header("Pragma: public");
+    header("Content-type: application/pdf");
+    header("Content-Disposition: inline; filename=".$_POST["rutaDoc"]."");
+    header("Content-Description: PHP Generated Data");
+    header("Content-Transfer-Encoding: binary");
+    header('Content-Length:' . filesize($file));
+    ob_clean();
+    flush();
+    while (!feof($fp)) {
+        $buff = fread($fp, 1024);
+        print $buff;
+    }
 ?>
