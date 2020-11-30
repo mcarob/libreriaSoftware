70256ae8aa970208549ba12ab30a77d0bc0585e4
sebastianGR429 <55076154+sebastianGR429@users.noreply.github.com>
2020-11-30 16:43:45 -0500

    cliente

--- controlador/ControladorAutor.php
+++ controlador/ControladorAutor.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once($_SERVER['DOCUMENT_ROOT'].'/libreriaSoftware/Modelo/daos/autorDAO.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/libreriaSoftware/modelo/daos/autorDAO.php');
 
 
 
