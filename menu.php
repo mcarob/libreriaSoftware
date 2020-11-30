b39fbad9ca5bb13b566004e0e355536b672ddab4
sebastianGR429 <55076154+sebastianGR429@users.noreply.github.com>
2020-11-30 13:03:39 -0500

    cliente

--- menu.php
+++ menu.php
@@ -12,44 +12,6 @@
                 <nav class="mainmenu__nav">
                     <ul class="meninmenu d-flex justify-content-start">
                         <li class="drop with--one--item"><a href="index.php">Inicio</a></li>
-                        <li class="drop"><a href="#">Documentos</a>
-                            <div class="megamenu mega02">
-                                <ul class="item item02">
-                                    <li class="title">Documentos Físicos</li>
-                                    <li><a href="shop-grid.html">Libros Físicos</a></li>
-                                    <li><a href="single-product.html">Ponencias Físicas</a></li>
-                                    <li><a href="single-product.html">Articulos Físicas</a></li>
-                                </ul>
-                                <ul class="item item02">
-                                    <li class="title">Documentos Digitales</li>
-                                    <li><a href="shop-grid.html">Libros Digitales</a></li>
-                                    <li><a href="single-product.html">Ponencias Digitales</a></li>
-                                    <li><a href="single-product.html">Articulos Digitales</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li class="drop"><a href="shop-grid.html">La Libreria</a>
-                            <div class="megamenu dropdown">
-                                <ul class="item item01">
-                                    <li class="title">Nuestros Servicios</li>
-                                    <li><a href="shop-grid.html">Reserva Online Documentos </a></li>
-                                    <li><a href="shop-grid.html">Descargas de Documentos digitales </a></li>
-                                    <li><a href="shop-grid.html">Salas de conferencia </a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li class="drop"><a href="shop-grid.html">Nuestras Sedes</a>
-                            <div class="megamenu mega02">
-                                <ul class="item item02">
-                                    <li class="title">Bogotá</li>
-                                    <li><a href="shop-grid.html">Suba Bulevar</a></li>
-                                    <li><a href="shop-grid.html">Unicentro</a></li>
-                                    <li><a href="shop-grid.html">Centro Mayor</a></li>
-                                </ul>
-                            </div>
-                        </li>
-
-                        <li><a href="contact.html">Contactanos</a></li>
                         <li><a href="login.php">Registarse/Iniciar Sesión</a></li>
                     </ul>
                 </nav>
@@ -66,38 +28,8 @@
             <div class="col-lg-12 d-none">
                 <nav class="mobilemenu__nav">
                     <ul class="meninmenu">
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="#">Pages</a>
-                            <ul>
-                                <li><a href="about.html">About Page</a></li>
-                                <li><a href="portfolio.html">Portfolio</a>
-                                    <ul>
-                                        <li><a href="portfolio.html">Portfolio</a></li>
-                                        <li><a href="portfolio-details.html">Portfolio Details</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="my-account.html">My Account</a></li>
-                                <li><a href="cart.html">Cart Page</a></li>
-                                <li><a href="checkout.html">Checkout Page</a></li>
-                                <li><a href="wishlist.html">Wishlist Page</a></li>
-                                <li><a href="error404.html">404 Page</a></li>
-                                <li><a href="faq.html">Faq Page</a></li>
-                                <li><a href="team.html">Team Page</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="shop-grid.html">Shop</a>
-                            <ul>
-                                <li><a href="shop-grid.html">Shop Grid</a></li>
-                                <li><a href="single-product.html">Single Product</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="blog.html">Blog</a>
-                            <ul>
-                                <li><a href="blog.html">Blog Page</a></li>
-                                <li><a href="blog-details.html">Blog Details</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="login.php">Iniciar/registrar</a></li>                        
                     </ul>
                 </nav>
             </div>
